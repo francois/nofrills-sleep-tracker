f419939a91fea6d3e527a1c05437f02b770a17d9
François Beausoleil <francois@teksol.info>
2016-08-01 18:30:15 +0000

    Correctly set Rollbar environment for easier tracking of errors

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ end
 configure do
   Rollbar.configure do |config|
     config.access_token = ENV.fetch("ROLLBAR_ACCESS_TOKEN")
+    config.environment  = ENV.fetch("STAGE", "development")
   end
 
   # Connect immediately, ensures database is correctly configured during boot, instead of when clients connect
