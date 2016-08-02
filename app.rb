1555fad66ff7c5e625b4d1bad29084d3f85b515f
François Beausoleil <francois@teksol.info>
2016-08-02 03:26:10 +0000

    Configure Rollbar framework

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ configure do
   Rollbar.configure do |config|
     config.access_token = ENV.fetch("ROLLBAR_ACCESS_TOKEN")
     config.environment  = ENV.fetch("STAGE", "development")
+    config.framework    = "Sinatra"
 
     # Use threaded async reporter
     config.use_thread
